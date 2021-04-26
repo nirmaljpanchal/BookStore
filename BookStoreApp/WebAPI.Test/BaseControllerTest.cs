f2a40a067c07f50822e41f56960e684432b54889
Nirmal Panchal <nirmaljpanchal@gmail.com>
2021-04-26 00:50:50 -0400

    Added test project

---
+++ BookStoreApp/WebAPI.Test/BaseControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using CoreAbstraction;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Test
+{
+    public class BaseControllerTest : IDisposable
+    {
+        public Mock<IBookService> MockBookService { get; set; }
+
+        public BaseControllerTest()
+        {
+            MockBookService = new Mock<IBookService>();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
--- BookStoreApp/WebAPI.Test/BookControllerTest.cs
+++ BookStoreApp/WebAPI.Test/BookControllerTest.cs
@@ -1,14 +1,42 @@
+using BookStoreApi.Controllers;
+using CoreAbstraction;
+using Model;
+using Moq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace WebAPI.Test
 {
-    public class BookControllerTest
+    public class BookControllerTest : IClassFixture<BaseControllerTest>
     {
+        private BooksController _controller;
+        public Mock<IBookService> MockBookService { get; set; }
+        private List<Book> _books;
+        public BookControllerTest(BaseControllerTest fixture)
+        {
+            MockBookService = fixture.MockBookService;
+            _books = new List<Book>();
+            _books.Add(new Book() { Title = "1st" });
+            _books.Add(new Book() { Title = "2nd" });
+            _controller = new BooksController(MockBookService.Object);
+        }
         [Fact]
         public void GetTest()
         {
+            MockBookService.Setup(x => x.GetAll()).Returns(_books);
+            var result = _controller.Get();
+            Assert.Equal(2, result.Count());
+        }
 
+        [Fact]
+        public void GetByIdTest()
+        {
+            var book = new Book() { Id=1, Title = "1st" };
+            MockBookService.Setup(x => x.GetById(1)).Returns(book);
+            var result = _controller.Get(1);
+            Assert.Equal(1, result.Id);
         }
     }
 }
